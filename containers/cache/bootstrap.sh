1912ed13db6e872090d5ff7cd991317dfada08ab
Luis SEVERINO <lfs.severino@gmail.com>
2017-04-07 11:37:11 +0200

    Drupal docker

--- containers/cache/bootstrap.sh
+++ containers/cache/bootstrap.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+# Install epel-release.
+yum install epel-release -y
+
+# Install git.
 yum install git -y
+
+# Install varnish.
+yum -y install varnish
+systemctl enable varnish
+systemctl start varnish
+varnishd -V
+
+# Install MemCache.
+yum -y install memcached
+memcached -h
+
+# Keep container running.
 tail -f /dev/null
--- containers/search/bootstrap.sh
+++ containers/search/bootstrap.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Install epel-release.
+yum install epel-release -y
+
+# Install git.
 yum install git -y
+
+# Keep container running.
 tail -f /dev/null
