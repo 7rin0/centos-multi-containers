6252cc280e2b70955bab39bb0c29c6ebf38f1d92
Luis SEVERINO <lfs.severino@gmail.com>
2017-04-12 02:53:02 +0200

    update centos setup

--- docker/containers/mail/bootstrap.sh
+++ docker/containers/mail/bootstrap.sh
@@ -6,7 +6,7 @@ echo "127.0.0.1 centos-setup.local" >> /etc/hosts
 postfix start
 
 # Send confirmation mail.
-echo "Subject: Postfix successfully installed" | sendmail -v -i -S mailhog:1025 lfs.severino@gmail.com lseverino@ausy-group.com
+echo "Subject: Postfix successfully installed" | sendmail -v -i  lfs.severino@gmail.com lseverino@ausy-group.com
 
 # Keep container running.
 tail -f /dev/null
