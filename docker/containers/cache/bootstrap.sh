aca24722bc8eb0e39f35184bf6037712c1b853eb
Luis SEVERINO <lfs.severino@gmail.com>
2017-04-15 17:14:09 +0200

    Add varnish, update dockerfiles and other adjustements

--- docker/containers/cache/bootstrap.sh
+++ docker/containers/cache/bootstrap.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Run Memcached.
+memcached -d -p 11211 -u memcached -m 64 -c 1024
+
+# Run Varnish.
+# https://pantheon.io/docs/varnish/
+varnishd -F -u varnish -f /etc/varnish/default.vcl
+
 # Keep container running.
 tail -f /dev/null
